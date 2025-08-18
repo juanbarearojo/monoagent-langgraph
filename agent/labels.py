8c63ccefcde4a11f98ba99bbfa595a4040c22ae8
barearojojuan@gmail.com <barearojojuan@gmail.com>
2025-08-18 13:52:26 +0200

    label to latin

---
+++ agent/labels.py
@@ -0,0 +1,14 @@
+from typing import Dict
+
+LABEL_TO_LATIN: Dict[str, str] = {
+    "Mantled_howler": "Alouatta palliata",
+    "Patas_monkey": "Erythrocebus patas",
+    "Bald_uakari": "Cacajao calvus",
+    "Japanese_macaque": "Macaca fuscata",
+    "Pygmy_marmoset": "Cebuella pygmaea",
+    "White_headed_capuchin": "Cebus capucinus",
+    "Silvery_marmoset": "Mico argentatus",
+    "Common_squirrel_monkey": "Saimiri sciureus",   # normalizado sin espacio
+    "Black_headed_night_monkey": "Aotus nigriceps",
+    "Nilgiri_langur": "Semnopithecus johnii",
+}
